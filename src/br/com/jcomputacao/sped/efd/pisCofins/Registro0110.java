92768e2c08cb68efa77996d5b83cb3eb88bda5b0
rafaelgalvao <rafaelgalvao@56544dd1-a790-4711-ae29-638a3112a704>
2011-11-08 23:50:52 +0000

    [update]corrigindo registro0100
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@10 56544dd1-a790-4711-ae29-638a3112a704

---
+++ src/br/com/jcomputacao/sped/efd/pisCofins/Registro0110.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.jcomputacao.sped.efd.pisCofins;
+
+/**
+ * 08/11/2011 21:31:27
+ * @author rafael.galvao
+ */
+public class Registro0110 {
+    public static String COD_INC_TRIB = "COD_INC_TRIB";
+    public static String IND_APRO_CRED = "IND_APRO_CRED";
+    public static String COD_TIPO_CONT = "COD_TIPO_CONT";
+    public static String IND_REG_CUM = "IND_REG_CUM";
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
--- test/br/com/jcomputacao/sped/efd/pisCofins/Registro0100Test.java
+++ test/br/com/jcomputacao/sped/efd/pisCofins/Registro0100Test.java
@@ -41,9 +41,23 @@ public class Registro0100Test {
     public void registro0100test(){
         Registro0100 reg = new Registro0100();
         LineModel line = reg.createModel();
-        
-        line.setFieldValue(Registro0100.BAIRRO, "as");
-        line.setFieldValue(Registro0100.REG, "12");
+      
+        line.setFieldValue(Registro0100.BAIRRO, "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        line.setFieldValue(Registro0100.REG, "1323");
+        line.setFieldValue(Registro0100.CEP, "22222222");
+        line.setFieldValue(Registro0100.CNPJ, "11111111111111");
+          String brancos = "  ";
+       StringBuffer sb = line.getRepresentation();
+        String expected = "02RETORNO01COBRANÇA       12341234567812345678Hermanoteu godofredo durvalino333SANTANDER      051109" +
+                "0000000000XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" +
+                "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" +
+                "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" + brancos + "123123456";
+
+
+        assertEquals(expected.length(), 400);
+        assertEquals(sb.toString().length(), 400);
+
+        assertEquals(expected, sb.toString());
         
     }
 }
