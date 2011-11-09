57e794ec13439aa3594b82a8413ff95b99014167
williamantunes <williamantunes@56544dd1-a790-4711-ae29-638a3112a704>
2011-11-09 10:31:32 +0000

    Teste da classe Registro0001, ja existente.
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@12 56544dd1-a790-4711-ae29-638a3112a704

---
+++ test/br/com/jcomputacao/sped/efd/pisCofins/Registro0001Test.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.jcomputacao.sped.efd.pisCofins;
+
+import br.com.jcomputacao.aristoteles.line.LineModel;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * 09/11/2011 08:23:46
+ * @author WILL
+ */
+public class Registro0001Test {
+    public Registro0001Test() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void registro0001test(){
+        Registro0001 reg = new Registro0001();
+        LineModel line = reg.createModel();
+        
+        line.setFieldValue(Registro0001.REG, "0001");
+        line.setFieldValue(Registro0001.IND_MOV, 0l);
+        
+        StringBuffer r = line.getRepresentation();
+        System.out.print(r);
+        
+    }
+
+    
+}
