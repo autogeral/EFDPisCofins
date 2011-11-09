a48424bd5a9603ca45df5addc64a896efa47ef44
joaobatista <joaobatista@56544dd1-a790-4711-ae29-638a3112a704>
2011-11-09 23:21:50 +0000

    [add] bloco c registro c380 teste
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@49 56544dd1-a790-4711-ae29-638a3112a704

---
+++ test/br/com/jcomputacao/sped/efd/pisCofins/RegistroC380Test.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.jcomputacao.sped.efd.pisCofins;
+
+import br.com.jcomputacao.aristoteles.line.LineModel;
+import java.text.Format;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author John
+ */
+public class RegistroC380Test {
+
+    public RegistroC380Test() throws ParseException {
+        RegistroC380 reg = new RegistroC380();
+        LineModel line = reg.createModel();
+        SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
+        Date data = sdf.parse("17121986");
+        line.setFieldValue(RegistroC380.REG, "0001");
+        line.setFieldValue(RegistroC380.COD_MOD, "02");
+        line.setFieldValue(RegistroC380.DT_DOC_INI, data);
+        line.setFieldValue(RegistroC380.DT_DOC_FIN, data);
+        line.setFieldValue(RegistroC380.NUM_DOC_INI, 123456L);
+        line.setFieldValue(RegistroC380.NUM_DOC_FIN, 123456L);
+        line.setFieldValue(RegistroC380.VL_DOC, 11111111111111111111D);
+        line.setFieldValue(RegistroC380.VL_DOC_CANC, 11111111111111111111D);
+
+        StringBuffer r = line.getRepresentation();
+        System.out.print(r);
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getNome method, of class RegistroC380.
+     */
+    @Test
+    public void testGetNome() {
+    }
+}
