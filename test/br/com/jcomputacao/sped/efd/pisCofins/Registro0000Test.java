0cacf48491e20742fcfff5580806aef314abbb0c
joaobatista <joaobatista@56544dd1-a790-4711-ae29-638a3112a704>
2011-11-09 22:13:10 +0000

    [add] classe registro0000test
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@44 56544dd1-a790-4711-ae29-638a3112a704

---
+++ test/br/com/jcomputacao/sped/efd/pisCofins/Registro0000Test.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.jcomputacao.sped.efd.pisCofins;
+
+import br.com.jcomputacao.aristoteles.line.LineModel;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author John
+ */
+public class Registro0000Test {
+    
+    public Registro0000Test() {
+        Registro0000 reg = new Registro0000();
+        LineModel line = reg.createModel();
+
+        line.setFieldValue(Registro0000.REG, "1323");
+        line.setFieldValue(Registro0000.COD_VER, 123L);
+        line.setFieldValue(Registro0000.TIPO_ESCRIT, 1L);
+        line.setFieldValue(Registro0000.IND_SIT_ESP, 4L);
+        line.setFieldValue(Registro0000.NUM_REC_ANTERIOR, "12345678901234567890123456789012345678901");
+        line.setFieldValue(Registro0000.DT_INI, 12345678L);
+        line.setFieldValue(Registro0000.DT_FIN, 98765432L);
+        line.setFieldValue(Registro0000.NOME, "ABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJ");
+        line.setFieldValue(Registro0000.CNPJ, 12345678901234L);
+        line.setFieldValue(Registro0000.UF, "RJ");
+        line.setFieldValue(Registro0000.COD_MUN, 1234567L);
+        line.setFieldValue(Registro0000.SUFRAMA, "QWERTYUIO");
+        line.setFieldValue(Registro0000.IND_NAT_PJ, 10L);
+        line.setFieldValue(Registro0000.IND_ATIV, 9L);
+
+        StringBuffer sb = line.getRepresentation();
+        //   String expected = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx    "+ "1323" + 22222222l + 1111111111111l;
+        System.out.print(sb);
+        // assertEquals(expected, sb.toString());
+        
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+}
