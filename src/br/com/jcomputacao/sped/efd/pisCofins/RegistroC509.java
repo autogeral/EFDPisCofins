f723522ce89f9b8d6097c7676d5f851c4ab5f95f
joaobatista <joaobatista@56544dd1-a790-4711-ae29-638a3112a704>
2011-11-11 00:21:10 +0000

    [add] bloco c registro c509
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@103 56544dd1-a790-4711-ae29-638a3112a704

---
+++ src/br/com/jcomputacao/sped/efd/pisCofins/RegistroC509.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.jcomputacao.sped.efd.pisCofins;
+
+import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldStringMaximumLengthArchetype;
+import br.com.jcomputacao.aristoteles.line.LineArchetype;
+
+/**
+ * 10/11/2011 22:15:51
+ * @author João Batista Rodrigues Porfirio
+ */
+public class RegistroC509 extends LineArchetype implements EfdPisCofinsRegistro {
+
+    public String getNome() {
+        return "PROCESSO REFERENCIADO";
+    }
+    public final static String REG = "REG";
+    public final static String NUM_PROC = "NUM_PROC";
+    public final static String IND_PROC = "IND_PROC";
+
+    public RegistroC509() {
+        addFieldArchetype(REG, new FieldStringFixedLengthArchetype(4));
+        addFieldArchetype(NUM_PROC, new FieldStringMaximumLengthArchetype(20));
+        addFieldArchetype(IND_PROC, new FieldStringFixedLengthArchetype(1));
+
+    }
+}
