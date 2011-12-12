32e1348303480b6e4b02452997cfa5ff362398b1
williamantunes <williamantunes@56544dd1-a790-4711-ae29-638a3112a704>
2011-12-12 13:20:49 +0000

    [add] comecando a implementar a impressao do Danfe a partir do XML do processamento.
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@186 56544dd1-a790-4711-ae29-638a3112a704

---
+++ test/br/com/jcomputacao/sped/nfe/danfe/DanfeImpressaoTest.java
@@ -0,0 +1,65 @@
+package br.com.jcomputacao.sped.nfe.danfe;
+
+import br.inf.portalfiscal.nfe.TNfeProc;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
+
+/**
+ *
+ * @author WILL
+ */
+public class DanfeImpressaoTest {
+
+    public DanfeImpressaoTest() {
+    }
+
+    public static void main(String a[]) {
+        String dir = "../exemplos/";
+        File fdir = new File(dir);
+        if (!fdir.exists()) {
+            System.out.println("Diretório não encontrado : " + fdir.getAbsolutePath());
+            System.out.flush();
+            return;
+        }
+        File[] files = fdir.listFiles(new FilenameFilter() {
+
+            @Override
+            public boolean accept(File dir, String name) {
+                return (name.toLowerCase().endsWith(".xml"));
+            }
+        });
+
+        List<TNfeProc> procs = new ArrayList<TNfeProc>();
+        for (File f : files) {
+            System.out.println("Tratando : " + f.getAbsolutePath());
+            try {
+                JAXBContext context = JAXBContext.newInstance("br.inf.portalfiscal.nfe");
+                Unmarshaller unmarshaller = context.createUnmarshaller();
+                FileInputStream fis = new FileInputStream(f);
+                TNfeProc proc = unmarshaller.unmarshal(new StreamSource(fis), TNfeProc.class).getValue();
+                fis.close();
+
+                if (proc != null) {
+                    procs.add(proc);
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).log(Level.SEVERE, null, ex);
+            } catch (JAXBException ex) {
+                Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    DanfeImpressao di = new DanfeImpressao(procs);
+    di.execute();
+}
