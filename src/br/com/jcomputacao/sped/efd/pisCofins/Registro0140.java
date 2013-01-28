4d31f44159b2fbf47ef8be1ee4327b9adb21eddf
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-28 18:08:39 +0000

    [update] insecao do delimitador de campos e ajuste na precisao dos campos numericos
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@221 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/pisCofins/Registro0140.java
+++ src/br/com/jcomputacao/sped/efd/pisCofins/Registro0140.java
@@ -4,42 +4,56 @@
  */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
-import br.com.jcomputacao.aristoteles.field.FieldDateFixedLengthArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldDecimalFixedLengthArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldFillerArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldIntegerFixedLengthArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.*;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
 /**
  * 09/11/2011 09:41:17
  * @author WILL
  */
 public class Registro0140 extends LineArchetype {
+    //01 - Fixo
     public static final String REG = "REG";
+    //02 - Código de identificação do estabelecimento
     public static final String COD_EST = "COD_EST";
+    //03 - Nome empresarial do estabelecimento
     public static final String NOME = "NOME";
+    //04 - Número de inscrição do estabelecimento no CNPJ.
     public static final String CNPJ = "CNPJ";
+    //05 - Sigla da unidade da federação do estabelecimento.
     public static final String UF = "UF";
+    //06 - Inscrição Estadual do estabelecimento, se contribuinte de ICMS.
     public static final String IE = "IE";
+    //07 - Código do município do domicílio fiscal do estabelecimento, 
+    //conforme a tabela IBGE
     public static final String COD_MUN = "COD_MUN";
+    //08 - Inscrição Municipal do estabelecimento, se contribuinte do ISS.
     public static final String IM = "IM";
+    //09 - Inscrição do estabelecimento na Suframa
     public static final String SUFRAMA = "SUFRAMA";
      
 
 
     public Registro0140(){
-        addFieldArchetype(REG, new FieldStringFixedLengthArchetype(4));
-        addFieldArchetype(COD_EST, new FieldStringFixedLengthArchetype(60));
-        addFieldArchetype(NOME, new FieldStringFixedLengthArchetype(100));
+        setName("Tabela de cadastro de estabelecimento");
+        setDelimiter("|");
+        //01
+        addFieldArchetype(REG, new FieldDefaultArchetype("0140"));
+        //02
+        addFieldArchetype(COD_EST, new FieldStringMaximumLengthArchetype(60));
+        //03
+        addFieldArchetype(NOME, new FieldStringMaximumLengthArchetype(100));
+        //04
         addFieldArchetype(CNPJ, new FieldIntegerFixedLengthArchetype(14));
+        //05
         addFieldArchetype(UF, new FieldStringFixedLengthArchetype(2));
-        addFieldArchetype(IE, new FieldStringFixedLengthArchetype(14));
+        //06
+        addFieldArchetype(IE, new FieldStringMaximumLengthArchetype(14));
+        //07
         addFieldArchetype(COD_MUN, new FieldIntegerFixedLengthArchetype(7));
-        addFieldArchetype(IM, new FieldStringFixedLengthArchetype(255));
+        //08
+        addFieldArchetype(IM, new FieldStringMaximumLengthArchetype(255));
+        //09
         addFieldArchetype(SUFRAMA, new FieldStringFixedLengthArchetype(9));
-
-    }
-    
+    }  
     
 }
