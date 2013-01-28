2a4c0e6c0651dcb04ee0721796bcd781e1374606
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-28 13:57:28 +0000

    [update] adaptacao para a validacao do documento - uso do delimitador pipe e tamanho dos campos
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@214 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/pisCofins/Registro0100.java
+++ src/br/com/jcomputacao/sped/efd/pisCofins/Registro0100.java
@@ -1,11 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
+import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldIntegerFixedLengthArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldStringMaximumLengthArchetype;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
 
 /**
@@ -13,36 +11,67 @@ import br.com.jcomputacao.aristoteles.line.LineArchetype;
  * @author rafael.galvao
  */
 public class Registro0100 extends LineArchetype {
-
+    
+    //01 
     public static String REG = "REG";
+    //02
     public static String NOME = "NOME";
+    //03
     public static String CPF = "CPF";
+    //04
     public static String CRC = "CRC";
+    //05
     public static String CNPJ = "CNPJ";
+    //06
     public static String CEP = "CEP";
+    //07
     public static String END = "END";
+    //08
     public static String NUM = "NUM";
+    //09
     public static String COMPL = "COMPL";
+    //10
     public static String BAIRRO = "BAIRRO";
+    //11
     public static String FONE = "FONE";
+    //12
     public static String FAX = "FAX";
+    //13
     public static String EMAIL = "EMAIL";
+    //14
     public static String COD_MUN = "COD_MUN";
 
     public Registro0100() {
-        addFieldArchetype(REG, new FieldStringFixedLengthArchetype(4));
-        addFieldArchetype(NOME, new FieldStringFixedLengthArchetype(100));
+        
+        setName("Dados do contabilista");
+        setDelimiter("|");
+        //01
+        addFieldArchetype(REG, new FieldDefaultArchetype("0100"));
+        //02
+        addFieldArchetype(NOME, new FieldStringMaximumLengthArchetype(100));
+        //03
         addFieldArchetype(CPF, new FieldIntegerFixedLengthArchetype(11));
-        addFieldArchetype(CRC, new FieldStringFixedLengthArchetype(15));
+        //04
+        addFieldArchetype(CRC, new FieldStringMaximumLengthArchetype(15));
+        //05
         addFieldArchetype(CNPJ, new FieldIntegerFixedLengthArchetype(14));
+        //06
         addFieldArchetype(CEP, new FieldIntegerFixedLengthArchetype(8));
-        addFieldArchetype(END, new FieldStringFixedLengthArchetype(60));
-        addFieldArchetype(NUM, new FieldStringFixedLengthArchetype(255));
-        addFieldArchetype(COMPL, new FieldStringFixedLengthArchetype(60));
-        addFieldArchetype(BAIRRO, new FieldStringFixedLengthArchetype(60));
+        //07
+        addFieldArchetype(END, new FieldStringMaximumLengthArchetype(60));
+        //08
+        addFieldArchetype(NUM, new FieldStringMaximumLengthArchetype(255));
+        //09
+        addFieldArchetype(COMPL, new FieldStringMaximumLengthArchetype(60));
+        //10
+        addFieldArchetype(BAIRRO, new FieldStringMaximumLengthArchetype(60));
+        //11
         addFieldArchetype(FONE, new FieldStringFixedLengthArchetype(10));
+        //12
         addFieldArchetype(FAX, new FieldStringFixedLengthArchetype(10));
-        addFieldArchetype(EMAIL, new FieldStringFixedLengthArchetype(255));
+        //13
+        addFieldArchetype(EMAIL, new FieldStringMaximumLengthArchetype(255));
+        //14
         addFieldArchetype(COD_MUN, new FieldIntegerFixedLengthArchetype(7));
     }
 }
