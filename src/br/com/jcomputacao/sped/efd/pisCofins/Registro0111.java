87b132e6cdbb76b68a1e2dc1589f0bc599b31793
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-28 17:44:34 +0000

    [update] insecao do delimitador de campos e ajuste na precisao dos campos numericos
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@219 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/pisCofins/Registro0111.java
+++ src/br/com/jcomputacao/sped/efd/pisCofins/Registro0111.java
@@ -4,12 +4,10 @@
  */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
-import br.com.jcomputacao.aristoteles.field.FieldDateFixedLengthArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldDecimalFixedLengthArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldFillerArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldIntegerFixedLengthArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.*;
+import br.com.jcomputacao.aristoteles.format.FormatFactory;
+import br.com.jcomputacao.aristoteles.format.FormatType;
+import br.com.jcomputacao.aristoteles.format.FormatWrapper;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
 
 /**
@@ -17,21 +15,52 @@ import br.com.jcomputacao.aristoteles.line.LineArchetype;
  * @author WILL
  */
 public class Registro0111 extends LineArchetype {
+     //01 - Fixo
      public static final String REG = "REG";
+     //02
      public static final String REC_BRU_NCUM_TRIB_MI= "REC_BRU_NCUM_TRIB_MI";
+     //03
      public static final String REC_BRU_NCUM_NT_MI = "REC_BRU_ NCUM_NT_MI";
+     //04
      public static final String REC_BRU_NCUM_EXP = "REC_BRU_ NCUM_EXP";
+     //05
      public static final String REC_BRU_CUM = "REC_BRU_CUM";
+     //06
      public static final String REC_BRU_TOTAL = "REC_BRU_TOTAL";
 
 
     public Registro0111(){
-        addFieldArchetype(REG, new FieldStringFixedLengthArchetype(4));
-        addFieldArchetype(REC_BRU_NCUM_TRIB_MI, new FieldDecimalFixedLengthArchetype(20,2));
-        addFieldArchetype(REC_BRU_NCUM_NT_MI, new FieldDecimalFixedLengthArchetype(20,2));
-        addFieldArchetype(REC_BRU_NCUM_EXP, new FieldDecimalFixedLengthArchetype(20,2));
-        addFieldArchetype(REC_BRU_CUM, new FieldDecimalFixedLengthArchetype(20,2));
-        addFieldArchetype(REC_BRU_TOTAL, new FieldDecimalFixedLengthArchetype(20,2));
+        setName(REG);
+        setDelimiter("|");
+        
+        FieldDecimalMaximumLengthArchetype fdf15 = new FieldDecimalMaximumLengthArchetype(15,2);
+        FormatWrapper fw = FormatFactory.getFormat(FormatType.DECIMAL);
+        fw.setReplaceComa(false);
+        fdf15.setFormat(fw);
+        //01
+        addFieldArchetype(REG, new FieldDefaultArchetype("0111"));
+        //02
+        /**Receita Bruta Não-Cumulativa - Tributada no Mercado Interno
+         */
+        addFieldArchetype(REC_BRU_NCUM_TRIB_MI, fdf15);
+        //03
+        /**Receita Bruta Não-Cumulativa – 
+         * Não Tributada no Mercado Interno 
+         * (Vendas com suspensão, alíquota zero, isenção e sem incidência das contribuições)
+         */
+        addFieldArchetype(REC_BRU_NCUM_NT_MI, fdf15);
+        //04
+        /**Receita Bruta Não-Cumulativa – Exportação
+         */
+        addFieldArchetype(REC_BRU_NCUM_EXP, fdf15);
+        //05
+        /**Receita Bruta Cumulativa
+         */
+        addFieldArchetype(REC_BRU_CUM, fdf15);
+        //06
+        /**Receita Bruta Total
+         */
+        addFieldArchetype(REC_BRU_TOTAL, fdf15);
 
     }
     
