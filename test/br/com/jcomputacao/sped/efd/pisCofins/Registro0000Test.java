1eb8d9b3e425c71ba5eff81e2ec30de345f3c304
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-28 13:27:31 +0000

    [update] testes para validacao do documento
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@212 56544dd1-a790-4711-ae29-638a3112a704

--- test/br/com/jcomputacao/sped/efd/pisCofins/Registro0000Test.java
+++ test/br/com/jcomputacao/sped/efd/pisCofins/Registro0000Test.java
@@ -13,6 +13,7 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
@@ -20,31 +21,45 @@ import org.junit.Test;
  */
 public class Registro0000Test {
     
+    private StringBuffer sb;
+    
     public Registro0000Test() throws ParseException {
         Registro0000 reg = new Registro0000();
         LineModel line = reg.createModel();
-SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
-        Date data = sdf.parse("17121986");
-        line.setFieldValue(Registro0000.REG, "1323");
+        SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
+        Date dataInicio = sdf.parse("26012013");
+        Date dataFim = sdf.parse("30012013");
+        //02
         line.setFieldValue(Registro0000.COD_VER, 123L);
+        //03
         line.setFieldValue(Registro0000.TIPO_ESCRIT, 1L);
+        //04
         line.setFieldValue(Registro0000.IND_SIT_ESP, 4L);
+        //05
         line.setFieldValue(Registro0000.NUM_REC_ANTERIOR, "12345678901234567890123456789012345678901");
-        line.setFieldValue(Registro0000.DT_INI, data);
-        line.setFieldValue(Registro0000.DT_FIN, data);
+        //06
+        line.setFieldValue(Registro0000.DT_INI, dataInicio);
+        //07
+        line.setFieldValue(Registro0000.DT_FIN, dataFim);
+        //08
         line.setFieldValue(Registro0000.NOME, "ABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJ");
+        //09
         line.setFieldValue(Registro0000.CNPJ, 12345678901234L);
+        //10
         line.setFieldValue(Registro0000.UF, "RJ");
+        //11
         line.setFieldValue(Registro0000.COD_MUN, 1234567L);
+        //12
         line.setFieldValue(Registro0000.SUFRAMA, "QWERTYUIO");
+        //13
         line.setFieldValue(Registro0000.IND_NAT_PJ, 10L);
+        //14
         line.setFieldValue(Registro0000.IND_ATIV, 9L);
 
-        StringBuffer sb = line.getRepresentation();
-        //   String expected = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx    "+ "1323" + 22222222l + 1111111111111l;
+        this.sb = line.getRepresentation();
+        String expected = "|0000|123|1|4|12345678901234567890123456789012345678901|26012013|30012013|ABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJ|12345678901234|RJ|1234567|QWERTYUIO|10|9|";
         System.out.print(sb);
-        // assertEquals(expected, sb.toString());
-        
+        assertEquals(expected, sb.toString());     
     }
 
     @BeforeClass
@@ -65,5 +80,12 @@ SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
 
     @Test
     public void testSomeMethod() {
+    }
+
+    /**
+     * @return the sb
+     */
+    public StringBuffer getSb() {
+        return sb;
     }
 }
