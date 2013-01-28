db6464f9a5e53b24db737dd0f1eebfaa8d1847ed
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-28 19:05:35 +0000

    [update] insecao do delimitador de campos e ajuste na precisao dos campos numericos
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@222 56544dd1-a790-4711-ae29-638a3112a704

--- test/br/com/jcomputacao/sped/efd/pisCofins/Registro0140Test.java
+++ test/br/com/jcomputacao/sped/efd/pisCofins/Registro0140Test.java
@@ -5,11 +5,13 @@
 package br.com.jcomputacao.sped.efd.pisCofins;
 
 import br.com.jcomputacao.aristoteles.line.LineModel;
+import java.text.ParseException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * 09/11/2011 09:51:25
@@ -17,6 +19,36 @@ import org.junit.Test;
  */
 public class Registro0140Test {
      public Registro0140Test() {
+     }
+
+    public String linhaRegistro0140Test() throws ParseException{
+        Registro0140 reg = new Registro0140();
+        LineModel line = reg.createModel();
+        
+        //02
+        line.setFieldValue(Registro0140.COD_EST, "Código de identificação do estabelecimento");
+        //03
+        line.setFieldValue(Registro0140.NOME, "Nome empresarial do estabelecimento");
+        //04
+        line.setFieldValue(Registro0140.CNPJ, 11111111111111L);
+        //05
+        line.setFieldValue(Registro0140.UF, "SP");
+        //06
+        line.setFieldValue(Registro0140.IE, "11111111111111");
+        //07
+        line.setFieldValue(Registro0140.COD_MUN, 1111111L);
+        //08
+        line.setFieldValue(Registro0140.IM, "Inscrição Municipal do estabelecimento, se contribuinte do ISS.");
+        //09
+        line.setFieldValue(Registro0140.SUFRAMA, "111111111");
+        
+        
+        StringBuffer sb = line.getRepresentation();
+        //String expected = "|0140|Código de identificação do estabelecimento|Nome empresarial do estabelecimento|11111111111111|SP|11111111111111|1111111|Inscrição Municipal do estabelecimento, se contribuinte do ISS.|111111111|";
+        //System.out.println(sb);
+        //System.out.print(expected);
+        //assertEquals (expected, sb.toString());
+        return sb.toString();
     }
 
     @BeforeClass
@@ -34,27 +66,8 @@ public class Registro0140Test {
     @After
     public void tearDown() {
     }
-
-    @Test
-    public void registro0001test(){
-        Registro0140 reg = new Registro0140();
-        LineModel line = reg.createModel();
-        
-        line.setFieldValue(Registro0140.REG, "0001");
-        line.setFieldValue(Registro0140.COD_EST, "111111111111111111111111111111111111111111111111111111111111");
-        line.setFieldValue(Registro0140.NOME, "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
-        line.setFieldValue(Registro0140.CNPJ, 1111111111111l);
-        line.setFieldValue(Registro0140.UF, "0l");
-        line.setFieldValue(Registro0140.IE, "1111111111111l");
-        line.setFieldValue(Registro0140.COD_MUN, 1111111l);
-        line.setFieldValue(Registro0140.IM, "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
-        line.setFieldValue(Registro0140.SUFRAMA, "111111111");
-        
-        
-        StringBuffer r = line.getRepresentation();
-        System.out.print(r);
-        
-    }
-
     
+     @Test
+    public void testSomeMethod() {
+    }
 }
