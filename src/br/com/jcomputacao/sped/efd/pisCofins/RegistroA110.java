72e15052983d41e50c086bc4b0369360b2624626
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-29 18:51:41 +0000

    [update] adaptacao dos campos as validacoes do documentos EFDPisCofins
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@256 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/pisCofins/RegistroA110.java
+++ src/br/com/jcomputacao/sped/efd/pisCofins/RegistroA110.java
@@ -1,10 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
-import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldStringMaximumLengthArchetype;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
 /**
@@ -12,15 +8,23 @@ import br.com.jcomputacao.aristoteles.line.LineArchetype;
  * @author Jonas
  */
 public class RegistroA110 extends LineArchetype{
+    //01 - Fixo
      public static String REG = "REG";
+     //02 - Código da informação complementar do documento fiscal 
+     //(Campo 02 do Registro 0450)
      public static String COD_INF = "COD_INF";
+     //03 - Informação Complementar do Documento Fiscal
      public static String TXT_COMPL = "TXT_COMPL";
     
     
      public RegistroA110(){
-     
-         addFieldArchetype(REG, new FieldStringFixedLengthArchetype(4));
+         setName("Complemento do documento - informação complementar da NF");
+         setDelimiter("|");
+         //01
+         addFieldArchetype(REG, new FieldDefaultArchetype("A110"));
+         //02
          addFieldArchetype(COD_INF, new FieldStringMaximumLengthArchetype(6));
+         //03
          addFieldArchetype(TXT_COMPL, new FieldStringMaximumLengthArchetype(255));
      }
 }
