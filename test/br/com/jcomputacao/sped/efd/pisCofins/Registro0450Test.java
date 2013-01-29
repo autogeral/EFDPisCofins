2d8fcb964658f0567e8f33cc425ffabaa62bca9a
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-29 15:31:50 +0000

    [update] novos testes baseados nas validacoes do documento EDFPisCofins
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@249 56544dd1-a790-4711-ae29-638a3112a704

--- test/br/com/jcomputacao/sped/efd/pisCofins/Registro0450Test.java
+++ test/br/com/jcomputacao/sped/efd/pisCofins/Registro0450Test.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
 import br.com.jcomputacao.aristoteles.line.LineModel;
@@ -21,33 +17,38 @@ public class Registro0450Test {
     public Registro0450Test() {
     }
 
-    @BeforeClass
+    public String linhaRegistro0450Test(){
+        Registro0450 reg = new Registro0450();
+        LineModel line = reg.createModel();
+        
+        //02
+        line.setFieldValue(Registro0450.COD_INF, "123456");
+        //03
+        line.setFieldValue(Registro0450.TXT, "Texto livre da informação complementar existente no documento fiscal, inclusive espécie de normas legais, poder normativo, número, capitulação, data e demais referências pertinentes com indicação referentes ao tributo.");
+        
+        StringBuffer sb = line.getRepresentation();
+//        System.out.print(sb);
+//        String expected = "|0450|123456|Texto livre da informação complementar existente no documento fiscal, inclusive espécie de normas legais, poder normativo, número, capitulação, data e demais referências pertinentes com indicação referentes ao tributo.|";
+//        assertEquals (expected, sb.toString());
+        return sb.toString();
+    }
+        @BeforeClass
     public static void setUpClass() throws Exception {
     }
 
     @AfterClass
     public static void tearDownClass() throws Exception {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
-
+    
     @Test
-    public void Registro0450Test(){
-        Registro0450 reg = new Registro0450();
-        LineModel line = reg.createModel();
-        
-        line.setFieldValue(Registro0450.REG, "0001");
-        line.setFieldValue(Registro0450.COD_INF, "aaaaaa");
-        line.setFieldValue(Registro0450.TXT, "llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
-);
-        StringBuffer a = line.getRepresentation();
-        System.out.print(a);
-        
+    public void testSomeMethod() {
     }
 }
