1b2528cb9f6ccd6b10ae6f3ded4e5795a52b4eee
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-29 15:32:11 +0000

    [update] novos testes baseados nas validacoes do documento EDFPisCofins
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@251 56544dd1-a790-4711-ae29-638a3112a704

--- test/br/com/jcomputacao/sped/efd/pisCofins/Registro0600Test.java
+++ test/br/com/jcomputacao/sped/efd/pisCofins/Registro0600Test.java
@@ -1,16 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
 import br.com.jcomputacao.aristoteles.line.LineModel;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
 /**
  *
  * @author rafael.galvao
@@ -20,6 +15,27 @@ public class Registro0600Test {
     public Registro0600Test() {
     }
 
+    public String linhaRegistro0600Test() throws ParseException{
+        Registro0600 reg = new Registro0600();
+        LineModel line = reg.createModel();
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
+        Date data = sdf.parse("29012013");
+        
+
+        //02
+        line.setFieldValue(Registro0600.DT_ALT, data);
+        //03
+        line.setFieldValue(Registro0600.COD_CCUS, "Código do centro de custos.");
+        //04
+        line.setFieldValue(Registro0600.CCUS, "Nome do centro de custos.");
+        
+        StringBuffer sb = line.getRepresentation();
+//        System.out.print(sb);
+//        String expected = "|0600|29012013|Código do centro de custos.|Nome do centro de custos.|";
+//        assertEquals (expected, sb.toString());   
+        return sb.toString();
+    }
     @BeforeClass
     public static void setUpClass() throws Exception {
     }
@@ -27,26 +43,16 @@ public class Registro0600Test {
     @AfterClass
     public static void tearDownClass() throws Exception {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
-
+    
     @Test
-    public void Registro0600Test(){
-        Registro0600 reg = new Registro0600();
-        LineModel line = reg.createModel();
-        
-        line.setFieldValue(Registro0600.REG, "0123");
-        line.setFieldValue(Registro0600.DT_ALT, 12345678L);
-        line.setFieldValue(Registro0600.COD_CCUS, "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
-        line.setFieldValue(Registro0600.CCUS, "    rafaelalexandredeoliveiragalvaojcomputacaovilanovaitusao");
-        StringBuffer a = line.getRepresentation();
-        System.out.print(a);
-               
+    public void testSomeMethod() {
     }
 }
