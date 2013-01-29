df3ba5ba67872202e4b84c74a8466309769b72c5
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-29 15:28:41 +0000

    [update] tratamento dos tipos de variaveis baseado no documento atualizado - EDFPisCofins
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@236 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/pisCofins/Registro0206.java
+++ src/br/com/jcomputacao/sped/efd/pisCofins/Registro0206.java
@@ -1,10 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
+import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldStringMaximumLengthArchetype;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
 
 /**
@@ -12,11 +10,17 @@ import br.com.jcomputacao.aristoteles.line.LineArchetype;
  * @author rafael.galvao
  */
 public class Registro0206 extends LineArchetype{
+    //01 - Fixo
     public static String REG = "REG";
+    //02 - Código do combustível, conforme tabela publicada pela ANP
     public static String COD_COMB = "COD_COMB";
     
     public Registro0206(){
-        addFieldArchetype(REG, new FieldStringFixedLengthArchetype(4));
-        addFieldArchetype(COD_COMB, new FieldStringFixedLengthArchetype(255));
+        setName("Código de produto conforme tabela ANP (COmbustíveis)");
+        setDelimiter("|");
+        //01
+        addFieldArchetype(REG, new FieldDefaultArchetype("0206"));
+        //02
+        addFieldArchetype(COD_COMB, new FieldStringMaximumLengthArchetype(255));
     }
 }
