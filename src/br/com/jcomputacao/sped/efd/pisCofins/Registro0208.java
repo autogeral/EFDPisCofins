3742c060eb7e918559db5f6eadbbc6fbbecd8aac
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-29 15:29:07 +0000

    [update] tratamento dos tipos de variaveis baseado no documento atualizado - EDFPisCofins
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@237 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/pisCofins/Registro0208.java
+++ src/br/com/jcomputacao/sped/efd/pisCofins/Registro0208.java
@@ -1,32 +1,49 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
-import br.com.jcomputacao.aristoteles.field.FieldDateFixedLengthArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldDecimalFixedLengthArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldFillerArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldIntegerFixedLengthArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldStringMaximumLengthArchetype;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
 /**
  * 09/11/2011 11:47:59
  * @author WILL
  */
 public class Registro0208 extends LineArchetype {
-        public static final String REG = "REG";
+    //01 - Fixo
+    public static final String REG = "REG";
+    //02
+    /**Código indicador da Tabela de Incidência, conforme Anexo III do Decreto nº 6.707/08: 
+     * 01 – Tabela I 
+     * 02 – Tabela II 
+     * 03 – Tabela III 
+     * 04 – Tabela IV 
+     * 05 – Tabela V
+     * 06 – Tabela VI 
+     * 07 – Tabela VII 
+     * 08– Tabela VIII 
+     * 09 – Tabela IX 
+     * 10 – Tabela X 
+     * 11 – Tabela XI 
+     * 12 – Tabela XII
+     */
     public static final String COD_TAB = "COD_TAB";
+    //03 - Código do grupo, conforme Anexo III do Decreto nº 6.707/08.
     public static final String COD_GRU= "DT_INI";
+    //04 - Marca Comercial
     public static final String MARCA_COM= "DT_FIM";
    
     
         public Registro0208(){
-        addFieldArchetype(REG, new FieldStringFixedLengthArchetype(4));
+        setName("Código de grupos por marca comercial - Refri (Bebidas frias)");
+        setDelimiter("|");
+        //01
+        addFieldArchetype(REG, new FieldDefaultArchetype("0208"));
+        //02
         addFieldArchetype(COD_TAB, new FieldStringFixedLengthArchetype(2));
+        //03
         addFieldArchetype(COD_GRU, new FieldStringFixedLengthArchetype(2));
-        addFieldArchetype(MARCA_COM, new FieldStringFixedLengthArchetype(60));
+        //04
+        addFieldArchetype(MARCA_COM, new FieldStringMaximumLengthArchetype(60));
         
     }
     
