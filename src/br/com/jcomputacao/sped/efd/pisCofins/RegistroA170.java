acc3e1cdeaba21b6f53c86966973a21c0d0cb415
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-29 18:52:13 +0000

    [update] adaptacao dos campos as validacoes do documentos EFDPisCofins
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@259 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/pisCofins/RegistroA170.java
+++ src/br/com/jcomputacao/sped/efd/pisCofins/RegistroA170.java
@@ -6,6 +6,9 @@ import br.com.jcomputacao.aristoteles.field.FieldIntegerFixedLengthArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldIntegerMaximumLengthArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldStringMaximumLengthArchetype;
+import br.com.jcomputacao.aristoteles.format.FormatFactory;
+import br.com.jcomputacao.aristoteles.format.FormatType;
+import br.com.jcomputacao.aristoteles.format.FormatWrapper;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
 
 /**
@@ -13,45 +16,94 @@ import br.com.jcomputacao.aristoteles.line.LineArchetype;
  * @author Jonas
  */
 public class RegistroA170 extends LineArchetype {
-
+    //01 - Fixo
     public static String REG = "REG";
+    //02 - Número seqüencial do item no documento fiscal
     public static String NUM_ITEM = "NUM_ITEM";
+    //03 - Código do item (campo 02 do Registro 0200)
     public static String COD_ITEM = "COD_ITEM";
+    //04 - Descrição complementar do item como adotado no documento fiscal
     public static String DESCR_COMPL = "DESCR_COMPL";
+    //05 - Valor total do item (mercadorias ou serviços)
     public static String VL_ITEM = "VL_ITEM";
+    //06 - Valor do desconto do item / Exclusão
     public static String VL_DESC = "VL_DESC";
+    //07 - Código da Base de Cálculo do Crédito, 
+    //conforme a Tabela indicada no item 4.3.7, caso seja 
+    //informado código representativo de crédito no 
+    //Campo 09 (CST_PIS) ou no Campo 13 (CST_COFINS).
     public static String NAT_BC_CRED = "NAT_BC_CRED";
+    //08
+    /**Indicador da origem do crédito: 
+     * 0 – Operação no Mercado Interno 
+     * 1 – Operação de Importação
+     */
     public static String IND_ORIG_CRED = "IND_ORIG_CRED";
+    //09 - Código da Situação Tributária referente ao PIS/PASEP – Tabela 4.3.3.
     public static String CST_PIS = "CST_PIS";
+    //10 - Valor da base de cálculo do PIS/PASEP.
     public static String VL_BC_PIS = "VL_BC_PIS";
+    //11 - Alíquota do PIS/PASEP (em percentual)
     public static String ALIQ_PIS = "ALIQ_PIS";
+    //12 - Valor do PIS/PASEP
     public static String VL_PIS = "VL_PIS";
+    //13 - Código da Situação Tributária referente ao COFINS – Tabela 4.3.4.
     public static String CST_COFINS = "CST_COFINS";
+    //14 - Valor da base de cálculo da COFINS
     public static String VL_BC_COFINS = "VL_BC_COFINS";
+    //15 - Alíquota do COFINS (em percentual)
     public static String ALIQ_COFINS = "ALIQ_COFINS";
+    //16 - Valor da COFINS
     public static String VL_COFINS = "VL_COFINS";
+    //17 - Código da conta analítica contábil debitada/creditada
     public static String COD_CTA = "COD_CTA";
+    //18 - Código do centro de custos
     public static String COD_CCUS = "COD_CCUS";
 
     public RegistroA170() {
-
+        setName("Complemento do documento - itens do documento");
+        setDelimiter("|");
+        FieldDecimalMaximumLengthArchetype fdf = new FieldDecimalMaximumLengthArchetype(15,2);
+        FormatWrapper fw = FormatFactory.getFormat(FormatType.DECIMAL);
+        fw.setReplaceComa(false);
+        fdf.setFormat(fw);
+        //01
         addFieldArchetype(REG, new FieldDefaultArchetype("A170"));
-        addFieldArchetype(NUM_ITEM , new FieldIntegerMaximumLengthArchetype(4)); 
-        addFieldArchetype(COD_ITEM , new FieldStringMaximumLengthArchetype(60));
-        addFieldArchetype(DESCR_COMPL , new FieldStringMaximumLengthArchetype(255));
-        addFieldArchetype(VL_ITEM , new FieldDecimalMaximumLengthArchetype(20,2));
-        addFieldArchetype(VL_DESC ,  new FieldDecimalMaximumLengthArchetype(20,2));
-        addFieldArchetype(NAT_BC_CRED , new FieldStringFixedLengthArchetype(2));
-        addFieldArchetype(IND_ORIG_CRED , new FieldStringFixedLengthArchetype(1));
-        addFieldArchetype(CST_PIS , new FieldIntegerFixedLengthArchetype(2));
-        addFieldArchetype(VL_BC_PIS , new FieldDecimalMaximumLengthArchetype(20, 2));
-        addFieldArchetype(ALIQ_PIS , new FieldDecimalMaximumLengthArchetype(20, 2));
-        addFieldArchetype(VL_PIS , new FieldDecimalMaximumLengthArchetype(20, 2));
-        addFieldArchetype(CST_COFINS , new FieldIntegerFixedLengthArchetype(2));
-        addFieldArchetype(VL_BC_COFINS , new FieldDecimalMaximumLengthArchetype(20, 2));
-        addFieldArchetype(ALIQ_COFINS , new FieldDecimalMaximumLengthArchetype(6,2));
-        addFieldArchetype(VL_COFINS , new FieldDecimalMaximumLengthArchetype(20, 2));
+        //02
+        addFieldArchetype(NUM_ITEM, new FieldIntegerMaximumLengthArchetype(4)); 
+        //03
+        addFieldArchetype(COD_ITEM, new FieldStringMaximumLengthArchetype(60));
+        //04
+        addFieldArchetype(DESCR_COMPL, new FieldStringMaximumLengthArchetype(255));
+        //05
+        addFieldArchetype(VL_ITEM, fdf);
+        //06
+        addFieldArchetype(VL_DESC,  fdf);
+        //07
+        addFieldArchetype(NAT_BC_CRED, new FieldStringFixedLengthArchetype(2));
+        //08
+        addFieldArchetype(IND_ORIG_CRED, new FieldStringFixedLengthArchetype(1));
+        //09
+        addFieldArchetype(CST_PIS, new FieldIntegerFixedLengthArchetype(2));
+        //10
+        addFieldArchetype(VL_BC_PIS, fdf);
+        //11
+        addFieldArchetype(ALIQ_PIS, fdf);
+        //12
+        addFieldArchetype(VL_PIS, fdf);
+        //13
+        addFieldArchetype(CST_COFINS, new FieldIntegerFixedLengthArchetype(2));
+        //14
+        addFieldArchetype(VL_BC_COFINS, fdf);
+        //15
+        FieldDecimalMaximumLengthArchetype fdf6 = new FieldDecimalMaximumLengthArchetype(8,2);
+        fdf6.setFormat(fw);
+        addFieldArchetype(ALIQ_COFINS , fdf6);
+        //16
+        addFieldArchetype(VL_COFINS , fdf);
+        //17
         addFieldArchetype(COD_CTA , new FieldStringMaximumLengthArchetype(60));
+        //18
         addFieldArchetype(COD_CCUS ,new FieldStringMaximumLengthArchetype(60));
     }
 }
