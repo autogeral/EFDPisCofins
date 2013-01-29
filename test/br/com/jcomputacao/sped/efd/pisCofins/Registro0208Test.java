4e22153540a0ec63e7a681e0027b0d9ee7c86d8c
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-29 15:31:29 +0000

    [update] novos testes baseados nas validacoes do documento EDFPisCofins
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@247 56544dd1-a790-4711-ae29-638a3112a704

--- test/br/com/jcomputacao/sped/efd/pisCofins/Registro0208Test.java
+++ test/br/com/jcomputacao/sped/efd/pisCofins/Registro0208Test.java
@@ -1,23 +1,34 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
 import br.com.jcomputacao.aristoteles.line.LineModel;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
 /**
  * 09/11/2011 11:53:18
  * @author WILL
  */
 public class Registro0208Test {
-     public Registro0208Test() {
+    
+    public Registro0208Test() {
     }
 
+    public String linhaRegistro0208Test(){
+        Registro0208 reg = new Registro0208();
+        LineModel line = reg.createModel();
+        
+        //02
+        line.setFieldValue(Registro0208.COD_TAB, "01");
+        //03
+        line.setFieldValue(Registro0208.COD_GRU, "SN");
+        //04
+        line.setFieldValue(Registro0208.MARCA_COM, "Marca comercial");
+       
+        StringBuffer sb = line.getRepresentation();
+//        System.out.print(sb);
+//        String expected = "|0208|01|SN|Marca comercial|";
+//        assertEquals (expected, sb.toString());
+        return sb.toString();                
+    }
     @BeforeClass
     public static void setUpClass() throws Exception {
     }
@@ -33,21 +44,8 @@ public class Registro0208Test {
     @After
     public void tearDown() {
     }
-
-    @Test
-    public void registro0208test(){
-        Registro0208 reg = new Registro0208();
-        LineModel line = reg.createModel();
-        
-        line.setFieldValue(Registro0208.REG, "0001");
-        line.setFieldValue(Registro0208.COD_TAB, "11");
-        line.setFieldValue(Registro0208.COD_GRU, "11");
-        line.setFieldValue(Registro0208.MARCA_COM, "111111111111111111111111111111111111111111111111111111111111");
-       
-        
-        StringBuffer r = line.getRepresentation();
-        System.out.print(r);
-        
-    }
     
+    @Test
+    public void testSomeMethod() {
+    }
 }
