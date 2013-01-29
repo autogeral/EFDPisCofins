fd7e1879ddf72d5a050df56ab0139f7c97080d38
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-29 18:51:12 +0000

    [update] adaptacao dos campos as validacoes do documentos EFDPisCofins
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@253 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/pisCofins/RegistroA001.java
+++ src/br/com/jcomputacao/sped/efd/pisCofins/RegistroA001.java
@@ -1,8 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.jcomputacao.sped.efd.pisCofins;
+import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
 
@@ -11,13 +8,21 @@ import br.com.jcomputacao.aristoteles.line.LineArchetype;
  * @author Jonas
  */
 public class RegistroA001 extends LineArchetype {
-    
+    //01 - Fixo
     public static String REG = "REG";
+    //02
+    /**Indicador de movimento: 
+     * 0 - Bloco com dados informados
+     * 1 - Bloco sem dados informados
+     */
     public static String IND_MOV = "IND_MOV";
     
     public RegistroA001(){
-        
-        addFieldArchetype(REG, new FieldStringFixedLengthArchetype(4));
+        setName("Abertura do bloco A");
+        setDelimiter("|");
+        //01
+        addFieldArchetype(REG, new FieldDefaultArchetype("A001"));
+        //02
         addFieldArchetype(IND_MOV, new FieldStringFixedLengthArchetype(1));
     }
    
