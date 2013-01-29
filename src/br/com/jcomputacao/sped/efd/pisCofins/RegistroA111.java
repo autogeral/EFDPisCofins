a6e5a06680a66609070a5c5df5b32770d72a38d7
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-29 18:51:54 +0000

    [update] adaptacao dos campos as validacoes do documentos EFDPisCofins
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@257 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/pisCofins/RegistroA111.java
+++ src/br/com/jcomputacao/sped/efd/pisCofins/RegistroA111.java
@@ -1,9 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
+import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldIntegerFixedLengthArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldStringMaximumLengthArchetype;
@@ -13,14 +10,26 @@ import br.com.jcomputacao.aristoteles.line.LineArchetype;
  * @author Jonas
  */
 public class RegistroA111 extends LineArchetype {
+    //01 - Fixo
     public static String REG = "REG";
+    //02 - Identificação do processo ou ato concessório
      public static String NUM_PROC = "NUM_PROC";
+     //03
+     /**Indicador da origem do processo: 
+      * 1 - Justiça Federal
+      * 3 – Secretaria da Receita Federal do Brasil 
+      * 9 - Outros
+      */
      public static String IND_PROC = "IND_PROC";
      
     public RegistroA111(){
-        
-         addFieldArchetype(REG, new FieldStringFixedLengthArchetype(4));
+        setName("Processo referenciado");
+        setDelimiter("|");
+        //01
+         addFieldArchetype(REG, new FieldDefaultArchetype("A111"));
+         //02
          addFieldArchetype(NUM_PROC, new FieldStringMaximumLengthArchetype(15));
+         //03
          addFieldArchetype(IND_PROC, new FieldStringFixedLengthArchetype(1));
      }
 }
