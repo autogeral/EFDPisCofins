066387816276f2e14a22a21b6dcd96c79f7ba8be
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-29 18:53:22 +0000

    [update] testes baseados nas validacoes do documento EFDPisCofins
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@262 56544dd1-a790-4711-ae29-638a3112a704

--- test/br/com/jcomputacao/sped/efd/pisCofins/RegistroA100Test.java
+++ test/br/com/jcomputacao/sped/efd/pisCofins/RegistroA100Test.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
 import br.com.jcomputacao.aristoteles.line.LineModel;
@@ -9,11 +5,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
 
 /**
  * 09/11/2011 19:53:17
@@ -24,6 +17,59 @@ public class RegistroA100Test {
     public RegistroA100Test() {
     }
 
+    public String linhaRegistroA100Test() throws ParseException {
+        RegistroA100 reg = new RegistroA100();
+        LineModel line = reg.createModel();
+        SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
+        Date data = sdf.parse("10101987");
+
+        //02
+        line.setFieldValue(RegistroA100.IND_OPER, "0");
+        //03
+        line.setFieldValue(RegistroA100.IND_EMIT, "1");
+        //04
+        line.setFieldValue(RegistroA100.COD_PART, "Código do participante (campo 02 do Registro 0150)");
+        //05
+        line.setFieldValue(RegistroA100.COD_SIT, 0);
+        //06
+        line.setFieldValue(RegistroA100.SER, "Série do documento");
+        //07
+        line.setFieldValue(RegistroA100.SUB, "Subsérie do doc");
+        //08
+        line.setFieldValue(RegistroA100.NUM_DOC, "Número do documento fiscal ou documento internacional");
+        //09
+        line.setFieldValue(RegistroA100.CHV_NFSE, "Chave/Código de Verificação da nota fiscal de serv elet");
+        //10
+        line.setFieldValue(RegistroA100.DT_DOC, data );
+        //11
+        line.setFieldValue(RegistroA100.DT_EXE_SERV, data);
+        //12
+        line.setFieldValue(RegistroA100.VL_DOC, 100.02);
+        //13
+        line.setFieldValue(RegistroA100.IND_PGTO, "1");
+        //14
+        line.setFieldValue(RegistroA100.VL_DESC, 99.85);
+        //15
+        line.setFieldValue(RegistroA100.VL_BC_PIS, 65284.15);
+        //16
+        line.setFieldValue(RegistroA100.VL_PIS, 212.12);
+        //17
+        line.setFieldValue(RegistroA100.VL_BC_COFINS, 3235.10);
+        //18
+        line.setFieldValue(RegistroA100.VL_COFINS, 2.05);
+        //19
+        line.setFieldValue(RegistroA100.VL_PIS_RET, 98754.02);
+        //20
+        line.setFieldValue(RegistroA100.VL_COFINS_RET, 9856.56);
+        //21
+        line.setFieldValue(RegistroA100.VL_ISS, 985.75);
+
+        StringBuffer sb = line.getRepresentation();
+//        System.out.print(sb);
+//        String expected = "|A100|0|1|Código do participante (campo 02 do Registro 0150)|00|Série do documento|Subsérie do doc|Número do documento fiscal ou documento internacional|Chave/Código de Verificação da nota fiscal de serv elet|10101987|10101987|100,02|1|99,85|65284,15|212,12|3235,10|2,05|98754,02|9856,56|985,75|";
+//        assertEquals (expected, sb.toString());
+        return sb.toString();
+    }
     @BeforeClass
     public static void setUpClass() throws Exception {
     }
@@ -39,44 +85,8 @@ public class RegistroA100Test {
     @After
     public void tearDown() {
     }
-
+    
     @Test
-    public void registroA100test() throws ParseException {
-        RegistroA100 reg = new RegistroA100();
-        LineModel line = reg.createModel();
-        SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
-        Date data = sdf.parse("10101987");
-        String Str60 = "";
-
-        while (Str60.length() <= 60) {
-            Random r = new Random();
-            Str60 += (char) r.nextInt();
-        }
-
-        line.setFieldValue(RegistroA100.REG, "0001");
-        line.setFieldValue(RegistroA100.IND_OPER, "0");
-        line.setFieldValue(RegistroA100.IND_EMIT, "1");
-        line.setFieldValue(RegistroA100.COD_PART, Str60);
-        line.setFieldValue(RegistroA100.COD_SIT, 00l);
-        line.setFieldValue(RegistroA100.SER, Str60.substring(20, 40));
-        line.setFieldValue(RegistroA100.SUB, Str60.substring(10, 30));
-        line.setFieldValue(RegistroA100.NUM_DOC, Str60);
-        line.setFieldValue(RegistroA100.CHV_NFSE, Str60);
-        line.setFieldValue(RegistroA100.DT_DOC, data );
-        line.setFieldValue(RegistroA100.DT_EXE_SERV, data);
-        line.setFieldValue(RegistroA100.VL_DOC, 100.02);
-        line.setFieldValue(RegistroA100.IND_PGTO, Str60);
-        line.setFieldValue(RegistroA100.VL_DESC, 99.85);
-        line.setFieldValue(RegistroA100.VL_BC_PIS, 65284.15);
-        line.setFieldValue(RegistroA100.VL_PIS, 212.12);
-        line.setFieldValue(RegistroA100.VL_BC_COFINS, 3235.10);
-        line.setFieldValue(RegistroA100.VL_COFINS, 02.05);
-        line.setFieldValue(RegistroA100.VL_PIS_RET, 98754.02);
-        line.setFieldValue(RegistroA100.VL_COFINS_RET, 9856.56);
-        line.setFieldValue(RegistroA100.VL_ISS, 985.75);
-
-        StringBuffer r = line.getRepresentation();
-        System.out.print(r);
-
+    public void testSomeMethod() {
     }
 }
