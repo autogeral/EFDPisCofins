e7ca792e00db9dc92bee24c6b7933d20acc0ed89
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-29 15:29:25 +0000

    [update] tratamento dos tipos de variaveis baseado no documento atualizado - EDFPisCofins
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@239 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/pisCofins/Registro0450.java
+++ src/br/com/jcomputacao/sped/efd/pisCofins/Registro0450.java
@@ -1,10 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
-import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldStringMaximumLengthArchetype;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
 
 /**
@@ -12,14 +9,27 @@ import br.com.jcomputacao.aristoteles.line.LineArchetype;
  * @author rafael.galvao
  */
 public class Registro0450 extends LineArchetype {
+    //01 - Fixo
     public static String REG = "REG";
+    //02 - Código da informação complementar do documento fiscal.
     public static String COD_INF = "COD_INF";
+    //03
+    /**Texto livre da informação complementar existente no documento 
+     * fiscal, inclusive espécie de normas legais, poder normativo, 
+     * número, capitulação, data e demais referências pertinentes 
+     * com indicação referentes ao tributo.
+     */
     public static String TXT = "TXT";
     
     public Registro0450(){
-        addFieldArchetype(REG, new FieldStringFixedLengthArchetype(4));
-        addFieldArchetype(COD_INF, new FieldStringFixedLengthArchetype(6));
-        addFieldArchetype(TXT,  new FieldStringFixedLengthArchetype(255));
+        setName("Tabela de informação complementar do documento fiscal");
+        setDelimiter("|");
+        //01
+        addFieldArchetype(REG, new FieldDefaultArchetype("0450"));
+        //02 
+        addFieldArchetype(COD_INF, new FieldStringMaximumLengthArchetype(6));
+        //03
+        addFieldArchetype(TXT,  new FieldStringMaximumLengthArchetype(255));
         
     }
   
