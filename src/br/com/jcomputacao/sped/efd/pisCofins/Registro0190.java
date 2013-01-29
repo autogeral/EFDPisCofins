b9d66255db3da85f66782aba707b8e88d0bfc9b4
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-29 15:28:06 +0000

    [update] tratamento dos tipos de variaveis baseado no documento atualizado - EDFPisCofins
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@233 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/pisCofins/Registro0190.java
+++ src/br/com/jcomputacao/sped/efd/pisCofins/Registro0190.java
@@ -1,15 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
-import br.com.jcomputacao.aristoteles.field.FieldDateFixedLengthArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldDecimalFixedLengthArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldFillerArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldIntegerFixedLengthArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.*;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
 
 /**
@@ -17,15 +8,22 @@ import br.com.jcomputacao.aristoteles.line.LineArchetype;
  * @author WILL
  */
 public class Registro0190 extends LineArchetype {
-    
+    //01 - Fixo
     public static final String REG = "REG";
+    //02 - Código da unidade de medida
     public static final String UNID = "UNID";
+    //03 - Descrição da unidade de medida
     public static final String DESCR= "DESCR";
     
     public Registro0190(){
-        addFieldArchetype(REG, new FieldStringFixedLengthArchetype(4));
-        addFieldArchetype(UNID, new FieldStringFixedLengthArchetype(6));
-        addFieldArchetype(DESCR, new FieldStringFixedLengthArchetype(255));
+        setName("Identificação das unidades de medida");
+        setDelimiter("|");
+        //01
+        addFieldArchetype(REG, new FieldDefaultArchetype("0190"));
+        //02
+        addFieldArchetype(UNID, new FieldStringMaximumLengthArchetype(6));
+        //03
+        addFieldArchetype(DESCR, new FieldStringMaximumLengthArchetype(255));
     }
     
 }
