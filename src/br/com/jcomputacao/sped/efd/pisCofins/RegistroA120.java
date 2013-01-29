8b87318193c5a166f41bcb4112be5ada6200fcc7
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-29 18:52:03 +0000

    [update] adaptacao dos campos as validacoes do documentos EFDPisCofins
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@258 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/pisCofins/RegistroA120.java
+++ src/br/com/jcomputacao/sped/efd/pisCofins/RegistroA120.java
@@ -1,36 +1,64 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
-import br.com.jcomputacao.aristoteles.field.FieldDateTimeFixedLengthArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldDecimalMaximumLengthArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldStringMaximumLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.*;
+import br.com.jcomputacao.aristoteles.format.FormatFactory;
+import br.com.jcomputacao.aristoteles.format.FormatType;
+import br.com.jcomputacao.aristoteles.format.FormatWrapper;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
-
 /**
  * 09/11/2011 21:43:36
  * @author Jonas
  */
 public class RegistroA120 extends LineArchetype {
 
+    //01 - Fixo
     public static String REG = "REG";
+    //02 - 
     public static String VL_TOT_SERV = "VL_TOT_SERV";
+    //03 - 
     public static String VL_BC_PIS = "VL_BC_PIS";
+    //04 - 
     public static String VL_PIS_IMP = "VL_PIS_IMP";
+    //05 - 
     public static String DT_PAG_PIS = "DT_PAG_PIS";
+    //06 - 
     public static String VL_BC_COFINS = "VL_BC_COFINS";
+    //07 - 
+    public static String VL_COFINS_IMP = "VL_COFINS_IMP";
+    //08 - 
+    public static String DT_PAG_COFINS = "DT_PAG_COFINS";
+    //09
+    /**Local da execução do serviço: 
+     * 0 – Executado no País
+     * 1 – Executado no Exterior, cujo resultado se verifique no País
+     */
+    public static String LOC_EXE_SERV = "LOC_EXE_SERV";
 
     public RegistroA120() {
-
-        addFieldArchetype(REG, new FieldStringFixedLengthArchetype(4));
-        addFieldArchetype(VL_TOT_SERV, new FieldDecimalMaximumLengthArchetype(20, 2));
-        addFieldArchetype(VL_BC_PIS, new FieldDecimalMaximumLengthArchetype(20, 2));
-        addFieldArchetype(VL_PIS_IMP, new FieldDecimalMaximumLengthArchetype(20, 2));
-        addFieldArchetype(DT_PAG_PIS, new FieldDateTimeFixedLengthArchetype());
-        addFieldArchetype(VL_BC_COFINS, new FieldDecimalMaximumLengthArchetype(20, 2));
-
+        setName("Informação complementar - Operações de importação");
+        setDelimiter("|");
+        
+        FieldDecimalMaximumLengthArchetype fdf = new FieldDecimalMaximumLengthArchetype(15,2);
+        FormatWrapper fw = FormatFactory.getFormat(FormatType.DECIMAL);
+        fw.setReplaceComa(false);
+        fdf.setFormat(fw);
+        //01
+        addFieldArchetype(REG, new FieldDefaultArchetype("A120"));
+        //02
+        addFieldArchetype(VL_TOT_SERV, fdf);
+        //03
+        addFieldArchetype(VL_BC_PIS, fdf);
+        //04
+        addFieldArchetype(VL_PIS_IMP, fdf);
+        //05
+        addFieldArchetype(DT_PAG_PIS, new FieldDateFixedLengthArchetype("ddMMyyyy"));
+        //06
+        addFieldArchetype(VL_BC_COFINS, fdf);
+        //07
+        addFieldArchetype(VL_COFINS_IMP, fdf);
+        //08
+        addFieldArchetype(DT_PAG_COFINS, new FieldDateFixedLengthArchetype("ddMMyyyy"));
+        //09
+        addFieldArchetype(LOC_EXE_SERV, new FieldStringFixedLengthArchetype(1));
     }
 }
