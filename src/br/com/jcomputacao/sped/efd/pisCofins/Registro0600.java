9c4f490a70f98a5c315dd9ac218be451ebce8a01
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-29 15:29:45 +0000

    [update] tratamento dos tipos de variaveis baseado no documento atualizado - EDFPisCofins
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@241 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/pisCofins/Registro0600.java
+++ src/br/com/jcomputacao/sped/efd/pisCofins/Registro0600.java
@@ -1,11 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
-import br.com.jcomputacao.aristoteles.field.FieldIntegerFixedLengthArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldDateFixedLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldStringMaximumLengthArchetype;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
 
 /**
@@ -13,17 +10,25 @@ import br.com.jcomputacao.aristoteles.line.LineArchetype;
  * @author rafael.galvao
  */
 public class Registro0600 extends LineArchetype{
+    //01 - Fixo
     public static String REG = "REG";
+    //02 - Data da inclusão/alteração.
     public static String DT_ALT = "DT_ALT";
+    //03 - Código do centro de custos.
     public static String COD_CCUS = "COD_CCUS";
+    //04 - Nome do centro de custos.
     public static String CCUS = "CCUS";
     
     public Registro0600(){
-        addFieldArchetype(REG, new FieldStringFixedLengthArchetype(4));
-        addFieldArchetype(DT_ALT, new FieldIntegerFixedLengthArchetype(8));
-        addFieldArchetype(COD_CCUS, new FieldStringFixedLengthArchetype(60));
-        addFieldArchetype(CCUS, new FieldStringFixedLengthArchetype(60));
-        
-        
+        setName("Centro de custos");
+        setDelimiter("|");
+        //01
+        addFieldArchetype(REG, new FieldDefaultArchetype("0600"));
+        //02
+        addFieldArchetype(DT_ALT, new FieldDateFixedLengthArchetype("ddMMyyyy"));
+        //03
+        addFieldArchetype(COD_CCUS, new FieldStringMaximumLengthArchetype(60));
+        //04
+        addFieldArchetype(CCUS, new FieldStringMaximumLengthArchetype(60));
     }
 }
