16f2d97924a6ad6e545bae7949bf62af0a43cc48
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-01-29 15:29:16 +0000

    [update] tratamento dos tipos de variaveis baseado no documento atualizado - EDFPisCofins
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@238 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/pisCofins/Registro0400.java
+++ src/br/com/jcomputacao/sped/efd/pisCofins/Registro0400.java
@@ -1,10 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
-import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldStringMaximumLengthArchetype;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
 
 /**
@@ -12,13 +9,21 @@ import br.com.jcomputacao.aristoteles.line.LineArchetype;
  * @author rafael.galvao
  */
 public class Registro0400 extends LineArchetype {
+    //01 - Fixo
     public static String REG = "REG";
+    //02 - Código da natureza da operação/prestação
     public static String COD_NAT = "COD_NAT";
+    //03 - Descrição da natureza da operação/prestação
     public static String DESCR_NAT = "DESCR_NAT";
     
     public Registro0400(){
-        addFieldArchetype(REG, new FieldStringFixedLengthArchetype(4));
-        addFieldArchetype(COD_NAT, new FieldStringFixedLengthArchetype(10));
-        addFieldArchetype(DESCR_NAT, new FieldStringFixedLengthArchetype(255));
+        setName("Tabela de natureza da operação/prestação");
+        setDelimiter("|");
+        //01
+        addFieldArchetype(REG, new FieldDefaultArchetype("0400"));
+        //02
+        addFieldArchetype(COD_NAT, new FieldStringMaximumLengthArchetype(10));
+        //03
+        addFieldArchetype(DESCR_NAT, new FieldStringMaximumLengthArchetype(255));
     }
 }
