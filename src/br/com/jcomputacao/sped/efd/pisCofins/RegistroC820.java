36e02b2addd47a3de4960d23f9ce31bc17925a9a
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-02-06 18:53:37 +0000

    [update] finalizacao da classe RegistroC820
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@298 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/pisCofins/RegistroC820.java
+++ src/br/com/jcomputacao/sped/efd/pisCofins/RegistroC820.java
@@ -1,6 +1,12 @@
 package br.com.jcomputacao.sped.efd.pisCofins;
 
+import br.com.jcomputacao.aristoteles.field.FieldDecimalMaximumLengthArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldIntegerFixedLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.FieldStringMaximumLengthArchetype;
+import br.com.jcomputacao.aristoteles.format.FormatFactory;
+import br.com.jcomputacao.aristoteles.format.FormatType;
+import br.com.jcomputacao.aristoteles.format.FormatWrapper;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
 
 /**
@@ -38,9 +44,74 @@ public class RegistroC820 extends LineArchetype{
     public RegistroC820 (){
         setName("Detalhamento do cupom fiscal eletrônico (código 59) - Pis/Pasep e Cofins apurado por unidade de medida de produto");
         setDelimiter("|");
+        
+        FormatWrapper fw = FormatFactory.getFormat(FormatType.DECIMAL);
+        fw.setReplaceComa(false);
+        
+        FormatWrapper fw3 = FormatFactory.getDecimalFormatWithPrecision(3);
+        fw3.setReplaceComa(false);
+        
+        FormatWrapper fw4 = FormatFactory.getDecimalFormatWithPrecision(4);
+        fw4.setReplaceComa(false);
+        
         //01
         addFieldArchetype(REG, new FieldDefaultArchetype("C820"));
         //02
-        
+        addFieldArchetype(CFOP, new FieldIntegerFixedLengthArchetype(4));
+        //03
+        FieldDecimalMaximumLengthArchetype f03 = new FieldDecimalMaximumLengthArchetype(15,2);
+        f03.setFormat(fw);
+        addFieldArchetype(VL_ITEM, f03);
+        //04
+        FieldStringMaximumLengthArchetype f04 = new FieldStringMaximumLengthArchetype(60);
+        f04.setNullableRepresentation("");
+        f04.setFullFillingNullable(false);
+        addFieldArchetype(COD_ITEM, f04);
+        //05
+        addFieldArchetype(CST_PIS, new FieldIntegerFixedLengthArchetype(2));
+        //06
+        FieldDecimalMaximumLengthArchetype f06 = new FieldDecimalMaximumLengthArchetype(16, 3);
+        f06.setFormat(fw3);
+        f06.setNullableRepresentation("");
+        f06.setFullFillingNullable(false);
+        addFieldArchetype(QUANT_BC_PIS, f06);
+        //07
+        FieldDecimalMaximumLengthArchetype f07 = new FieldDecimalMaximumLengthArchetype(17, 4);
+        f07.setFormat(fw4);
+        f07.setNullableRepresentation("");
+        f07.setFullFillingNullable(false);
+        addFieldArchetype(ALIQ_PIS_QUANT, f07);
+        //08
+        FieldDecimalMaximumLengthArchetype f08 = new FieldDecimalMaximumLengthArchetype(15, 2);
+        f08.setFormat(fw);
+        f08.setNullableRepresentation("");
+        f08.setFullFillingNullable(false);
+        addFieldArchetype(VL_PIS, f08);
+        //09
+        FieldIntegerFixedLengthArchetype f09 = new FieldIntegerFixedLengthArchetype(2);
+        addFieldArchetype(CST_COFINS, f09);
+        //10
+        FieldDecimalMaximumLengthArchetype f10 = new FieldDecimalMaximumLengthArchetype(16, 3);
+        f10.setFormat(fw3);
+        f10.setNullableRepresentation("");
+        f10.setFullFillingNullable(false);
+        addFieldArchetype(QUANT_BC_COFINS, f10);
+        //11
+        FieldDecimalMaximumLengthArchetype f11 = new FieldDecimalMaximumLengthArchetype(17,4);
+        f11.setFormat(fw4);
+        f11.setNullableRepresentation("");
+        f11.setFullFillingNullable(false);
+        addFieldArchetype(ALIQ_COFINS_QUANT, f11);
+        //12
+        FieldDecimalMaximumLengthArchetype f12 = new FieldDecimalMaximumLengthArchetype(15, 2);
+        f12.setFormat(fw);
+        f12.setNullableRepresentation("");
+        f12.setFullFillingNullable(false);
+        addFieldArchetype(VL_COFINS, f12);
+        //13
+        FieldStringMaximumLengthArchetype f13 = new FieldStringMaximumLengthArchetype(60);
+        f13.setNullableRepresentation("");
+        f13.setFullFillingNullable(false);
+        addFieldArchetype(COD_CTA, f13);
     }
 }
