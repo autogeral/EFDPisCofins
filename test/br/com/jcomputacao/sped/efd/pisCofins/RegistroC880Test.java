4497482fdcd50a6ef440fd6d063d21d3064f2c91
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-02-06 18:59:20 +0000

    [add] criacao da classe de teste RegistroC880Test
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@300 56544dd1-a790-4711-ae29-638a3112a704

---
+++ test/br/com/jcomputacao/sped/efd/pisCofins/RegistroC880Test.java
@@ -0,0 +1,24 @@
+package br.com.jcomputacao.sped.efd.pisCofins;
+
+import br.com.jcomputacao.aristoteles.line.LineModel;
+
+/**
+ * Feb 6, 2013 4:55:11 PM
+ * @author Jennifer
+ */
+public class RegistroC880Test {
+    RegistroC880 reg = new RegistroC880();
+    LineModel line = reg.createModel();
+    //02 - 
+        //03 - 
+        //04 - 
+        //05 - 
+        //06 - 
+        //07 - 
+        //08 - 
+        //09 - 
+        //10 - 
+        //11 - 
+        //12 -
+        //13 -
+}
