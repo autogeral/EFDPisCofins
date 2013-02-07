2213eeaa4d1efc3d9a14b604335af390300957bb
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-02-07 18:58:40 +0000

    [update] ajuste das classes de testes aos padroes definidos no documendo EFD
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@317 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/pisCofins/RegistroD501.java
+++ src/br/com/jcomputacao/sped/efd/pisCofins/RegistroD501.java
@@ -1,12 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
-import br.com.jcomputacao.aristoteles.field.FieldDecimalMaximumLengthArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldIntegerMaximumLengthArchetype;
-import br.com.jcomputacao.aristoteles.field.FieldStringMaximumLengthArchetype;
+import br.com.jcomputacao.aristoteles.field.*;
+import br.com.jcomputacao.aristoteles.format.FormatFactory;
+import br.com.jcomputacao.aristoteles.format.FormatType;
+import br.com.jcomputacao.aristoteles.format.FormatWrapper;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
 
 /**
@@ -15,24 +12,69 @@ import br.com.jcomputacao.aristoteles.line.LineArchetype;
  */
 public class RegistroD501 extends LineArchetype {
 
+    //01 - Fixo
     public static String REG = "REG";
+    //02 - Código da Situação Tributária referente ao PIS/PASEP
     public static String CST_PIS = "CST_PIS";
+    //03 - Valor Total dos Itens (Serviços)
     public static String VL_ITEM = "VL_ITEM";
+    //04 - Código da Base de Cálculo do Crédito, conforme a Tabela indicada no item 4.3.7.
     public static String NAT_BC_CRED = "NAT_BC_CRED";
+    //05 - Valor da base de cálculo do PIS/PASEP
     public static String VL_BC_PIS = "VL_BC_PIS";
+    //06 - Alíquota do PIS/PASEP (em percentual)
     public static String ALIQ_PIS = "ALIQ_PIS";
+    //07 - Valor do PIS/PASEP
     public static String VL_PIS = "VL_PIS";
+    //08 - Código da conta analítica contábil debitada/creditada
     public static String COD_CTA = "COD_CTA";
 
     public RegistroD501() {
-        addFieldArchetype(REG, new FieldStringMaximumLengthArchetype(4));
-        addFieldArchetype(CST_PIS, new FieldIntegerMaximumLengthArchetype(2));
-        addFieldArchetype(VL_ITEM, new FieldDecimalMaximumLengthArchetype(20,2));
-        addFieldArchetype(NAT_BC_CRED, new FieldStringMaximumLengthArchetype(2));
-        addFieldArchetype(VL_BC_PIS, new FieldDecimalMaximumLengthArchetype(20,2));
-        addFieldArchetype(ALIQ_PIS, new FieldDecimalMaximumLengthArchetype(8,4));
-        addFieldArchetype(VL_PIS, new FieldDecimalMaximumLengthArchetype(20,2));
-        addFieldArchetype(COD_CTA, new FieldStringMaximumLengthArchetype(60));
+        setName("Complemento da operação (códigos 21 e 22) - Pis/Pasep");
+        setDelimiter("|");
+        
+        FormatWrapper fw = FormatFactory.getFormat(FormatType.DECIMAL);
+        fw.setReplaceComa(false);
+        
+        FormatWrapper fw4 = FormatFactory.getDecimalFormatWithPrecision(4);
+        fw4.setReplaceComa(false);
+        
+        //01
+        addFieldArchetype(REG, new FieldDefaultArchetype("D501"));
+        //02
+        addFieldArchetype(CST_PIS, new FieldIntegerFixedLengthArchetype(2));
+        //03
+        FieldDecimalMaximumLengthArchetype f03 = new FieldDecimalMaximumLengthArchetype(15,2);
+        f03.setFormat(fw);
+        addFieldArchetype(VL_ITEM, f03);
+        //04
+        FieldStringFixedLengthArchetype f04 = new FieldStringFixedLengthArchetype(2);
+        f04.setNullableRepresentation("");
+        f04.setFullFillingNullable(false);
+        addFieldArchetype(NAT_BC_CRED, f04);
+        //05
+        FieldDecimalMaximumLengthArchetype f05 = new FieldDecimalMaximumLengthArchetype(15,2);
+        f05.setFormat(fw);
+        f05.setNullableRepresentation("");
+        f05.setFullFillingNullable(false);
+        addFieldArchetype(VL_BC_PIS, f05);
+        //06
+        FieldDecimalMaximumLengthArchetype f06 = new FieldDecimalMaximumLengthArchetype(13,4);
+        f06.setFormat(fw4);
+        f06.setNullableRepresentation("");
+        f06.setFullFillingNullable(false);
+        addFieldArchetype(ALIQ_PIS, f06);
+        //07
+        FieldDecimalMaximumLengthArchetype f07 = new FieldDecimalMaximumLengthArchetype(15,2);
+        f07.setFormat(fw);
+        f07.setNullableRepresentation("");
+        f07.setFullFillingNullable(false);
+        addFieldArchetype(VL_PIS, f07);
+        //08
+        FieldStringMaximumLengthArchetype f08 = new FieldStringMaximumLengthArchetype(60);
+        f08.setNullableRepresentation("");
+        f08.setFullFillingNullable(false);
+        addFieldArchetype(COD_CTA, f08);
     }
      
 }
