9d806390b2499791a57731603cea1db26ec31389
viniciusdiana <viniciusdiana@56544dd1-a790-4711-ae29-638a3112a704>
2013-02-11 17:44:10 +0000

    [update] ajuste na classe
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@335 56544dd1-a790-4711-ae29-638a3112a704

--- src/br/com/jcomputacao/sped/efd/pisCofins/Registro1001.java
+++ src/br/com/jcomputacao/sped/efd/pisCofins/Registro1001.java
@@ -4,6 +4,7 @@
  */
 package br.com.jcomputacao.sped.efd.pisCofins;
 
+import br.com.jcomputacao.aristoteles.field.FieldDefaultArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldIntegerFixedLengthArchetype;
 import br.com.jcomputacao.aristoteles.field.FieldStringFixedLengthArchetype;
 import br.com.jcomputacao.aristoteles.line.LineArchetype;
@@ -14,11 +15,21 @@ import br.com.jcomputacao.aristoteles.line.LineArchetype;
  */
 public class Registro1001 extends LineArchetype {
     
-        public static String REG = "REG";
+    /*
+     * 1 - Texto fixo contendo "1001" 
+     */
+       public static String REG = "REG";
+       
+    /*
+      * 2 - Indicador de movimento: 0 - Bloco com dados informados; 1 - Bloco sem dados informados 
+
+     */
     public static String IND_MOV = "IND_MOV";
     
     public Registro1001(){
-        addFieldArchetype(REG, new FieldStringFixedLengthArchetype(4));
+        setName("Processo Referenciado - acao judicial");
+        setDelimiter("|");
+        addFieldArchetype(REG, new FieldDefaultArchetype("1001"));
         addFieldArchetype(IND_MOV, new FieldIntegerFixedLengthArchetype(1));
     }
     
