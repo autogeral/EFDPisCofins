3a9e349f9def18b563ca29276b1101277414c6f6
viniciusdiana <viniciusdiana@56544dd1-a790-4711-ae29-638a3112a704>
2013-02-11 17:43:57 +0000

    [update] ajuste na classe de testes
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@334 56544dd1-a790-4711-ae29-638a3112a704

--- test/br/com/jcomputacao/sped/efd/pisCofins/Registro1001Test.java
+++ test/br/com/jcomputacao/sped/efd/pisCofins/Registro1001Test.java
@@ -5,10 +5,7 @@
 package br.com.jcomputacao.sped.efd.pisCofins;
 
 import br.com.jcomputacao.aristoteles.line.LineModel;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 /**
@@ -16,34 +13,20 @@ import org.junit.Test;
  * @author WILL
  */
 public class Registro1001Test {
-        public Registro1001Test() {
-    }
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
     @Test
     public void Registro1001Test(){
         Registro1001 reg = new Registro1001();
-        LineModel line = reg.createModel();
-        
-        line.setFieldValue(Registro1001.REG, "1234");
+        LineModel line = reg.createModel();         
         line.setFieldValue(Registro1001.IND_MOV,1L);
-        StringBuffer a = line.getRepresentation();
-        System.out.print(a);
+        StringBuffer sb = line.getRepresentation();
+        String result = sb.toString();
+        String expected = "|1001|1|";
+        
+        System.out.println("Excecting ....  : " + expected);
+        System.out.println("Result    ....  : " + result);
+        assertEquals(expected, result);
     }
     
 }
