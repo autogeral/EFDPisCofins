142ae11d5ec7105057dcb18ccfca20361e584d72
jennifer <jennifer@56544dd1-a790-4711-ae29-638a3112a704>
2013-02-19 19:56:29 +0000

    [add] insercao da classe que representa o contabilista responsavel pela escrituracao do EFDContribuicoes
    git-svn-id: https://svn.autogeral.com.br/sped/EFDPisCofins@400 56544dd1-a790-4711-ae29-638a3112a704

---
+++ src/br/com/jcomputacao/sped/efd/pisCofins/ContabilistaEfdPisCofins.java
@@ -0,0 +1,21 @@
+package br.com.jcomputacao.sped.efd.pisCofins;
+
+/**
+ *
+ * @author Jennifer
+ */
+public interface ContabilistaEfdPisCofins {
+    public String getNome();  
+    public long getCpf();
+    public String getCrc();
+    public long getCnpj();
+    public int getCep();
+    public String getEndereco();
+    public String getNumero();
+    public String getComplemento();
+    public String getBairro();
+    public String getTelefone();
+    public String getFax();
+    public String getEmail();
+    public int getCodigoMunicipio();
+}
